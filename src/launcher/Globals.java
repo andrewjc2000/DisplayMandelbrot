7fcae76f5b6170063cb4c40b1bedd2e3a9a55484
Andrew <AndrewJChafos@gmail.com>
2017-09-12 09:31:04 -0400

    Whoops forgot to add this file
    See previous commit

--- src/launcher/Globals.java
+++ src/launcher/Globals.java
@@ -5,5 +5,6 @@ import graphics.CustomColorScheme;
 public class Globals {
     public static int progress, max, frameWidth, frameHeight, mouseX, mouseY, maxIt;
     public static double minR, maxR, minI, maxI;
+    public static boolean textBoxVisible, textBoxFadingOut;
     public static CustomColorScheme cols;
 }
